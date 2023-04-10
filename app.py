ac765571301bf5cbe917544a25d1a9f6b4f56df2
jjderz <89997839+jjderz@users.noreply.github.com>
2023-04-09 23:21:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-import streamlit as st
-from transformers import AutoTokenizer
-from transformers import (
-    TFAutoModelForSequenceClassification as AutoModelForSequenceClassification,
-)
-from transformers import pipeline
+import streamlit as s
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, pipeline
 
-st.title("Toxic Tweets")
+s.title("Toxic Tweets")
 
-demo = """Just took a stroll through the park and the sun is shining bright ☀️🌳 There's something so rejuvenating about being surrounded by nature! Hope you all have a wonderful day! 🌸😊 #grateful #happiness #naturelovers#flexinonthekids"""
+default_text = """Just took a stroll through the park and the sun is shining bright ☀️🌳 There's something so rejuvenating about being surrounded by nature! Hope you all have a wonderful day! 🌸😊 #grateful #happiness #naturelovers#flexinonthekids"""
 
-text = st.text_area("Input text", demo, height=275)
+input_text = s.text_area("Input text", default_text, height=275)
 
-model_name = st.selectbox(
+model_choice = s.selectbox(
     "Select the model you want to use below.",
     (
         "distilbert-base-uncased-finetuned-sst-2-english",
     ),
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-clf = pipeline(
-    "sentiment-analysis", model=model, tokenizer=tokenizer, return_all_scores=True
+token = AutoTokenizer.from_pretrained(model_choice)
+sequence_classifier = TFAutoModelForSequenceClassification.from_pretrained(model_choice)
+sentiment_pipeline = pipeline(
+    "sentiment-analysis", model=sequence_classifier, tokenizer=token, return_all_scores=True
 )
 
-input = tokenizer(text, return_tensors="tf")
+tensor_input = token(input_text, return_tensors="tf")
 
-if st.button("Submit", type="primary"):
-    results = clf(text)[0]
-    st.write(f"The sentiment is {results}.")
+if s.button("Submit", type="primary"):
+    sentiment_results = sentiment_pipeline(input_text)[0]
+    s.write(f"The sentiment is {sentiment_results}.")
