f79c79198df109b5969d337a2419b4897555a988
jjderz <89997839+jjderz@users.noreply.github.com>
2023-04-27 19:42:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Toxic Comment Classifier")
 
 sample_text = "I hate you, you suck."
 
-tweet_text = ""
+comment = ""
 proceed = False
 selected_model = ""
 col1, col2, col3 = st.columns([2,1,1])
@@ -17,7 +17,7 @@ col1, col2, col3 = st.columns([2,1,1])
 with st.container():
     selected_model = st.selectbox(
         "Choose the desired model from the dropdown menu.",
-        ("ogtega/tweet-toxicity-classifier",),
+        ("jjderz/toxic-classifier",),
     )
     proceed = st.button("Submit", type="primary", use_container_width=True)
 
@@ -29,7 +29,7 @@ classifier = pipeline(
 
 with col1:
     st.subheader("Comment")
-    tweet_text = st.text_area("Enter text", sample_text, height=275)
+    comment = st.text_area("Enter text", sample_text, height=275)
 
 with col2:
     st.subheader("Category")
@@ -38,10 +38,10 @@ with col3:
     st.subheader("Likelihood")
 
 
-input_data = token_processor(tweet_text, return_tensors="tf")
+input_data = token_processor(comment, return_tensors="tf")
 
 if proceed:
-    result_data = dict(d.values() for d in classifier(tweet_text)[0])
+    result_data = dict(d.values() for d in classifier(comment)[0])
     categories = {k: result_data[k] for k in result_data.keys() if not k == "toxic"}
 
     top_category = max(categories, key=categories.get)
@@ -53,9 +53,9 @@ if proceed:
         st.write(f"#### **{categories[top_category]:.2f}%**")
 
     if result_data["toxic"] < 0.5:
-        st.success("This tweet is likely not harmful!", icon=":white_check_mark:")
+        st.success("This comment isn't harmful!", icon=":white_check_mark:")
     else:
-        st.warning("This tweet might be harmful.", icon=":warning:")
+        st.warning("This comment is harmful.", icon=":warning:")
     
     expand_section = st.expander("Detailed output")
     expand_section.write(result_data)
